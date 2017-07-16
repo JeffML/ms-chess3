3ddb2c2e3d276a20817afb731e0d8b2109c4de69
Jeff Lowery <jlowery2663@gmail.com>
2017-07-16 11:13:16 -0700

    nothing blowed up

--- services/helpers/legalMovesWithKing.js
+++ services/helpers/legalMovesWithKing.js
@@ -4,6 +4,11 @@ module.exports = function (boardAndPiece, candidateMoves, reply) {
     //temporarily remove the K to avoid cycles
     boardAndPiece.board.removePiece(boardAndPiece.piece);
 
+    function canCastle(king, rook, intervening, opposing) {
+        const canCastle = !king.hasMoved && rook && !rook.hasMoved;
+        return canCastle;
+    }
+
     this.use(require('../SquareControl'))
 
     this.act({
@@ -16,18 +21,34 @@ module.exports = function (boardAndPiece, candidateMoves, reply) {
             reply(err);
             return;
         }
+
+        const king = boardAndPiece.piece;
         // console.log(opposing.controlled)
         // add the removed K back in
-        boardAndPiece.board.addPiece(boardAndPiece.piece);
+        boardAndPiece.board.addPiece(king);
         const filteredMoves = candidateMoves.moves.filter(m =>
             !!!opposing.controlled.find(o => o.rank === m.rank && o.file === m.file)
         )
 
-        const kingSq = boardAndPiece.piece.position;
+        const kingSq = king.position;
         const inCheck = !!opposing.controlled.find(o => o.rank === kingSq.rank && o.file === kingSq.file)
 
-        boardAndPiece.piece.inCheck = inCheck;
-        boardAndPiece.piece.checkMated = (inCheck && filteredMoves.length === 0)
+        king.inCheck = inCheck;
+
+        king.canQSideCastle = king.canKSideCastle = !(king.inCheck || king.hasMoved);
+
+        const rank = king.color === 'W' ? '1' : '8';
+        let rook = boardAndPiece.board.pieceAt(`a${rank}`);
+        let intervening = [`b${rank}`, `c${rank}`, `d${rank}`]
+
+        king.canQSideCastle = canCastle(king, rook, intervening, opposing)
+
+        rook = boardAndPiece.board.pieceAt(`h${rank}`);
+        intervening = [`f${rank}`, `g${rank}`]
+
+        king.canKSideCastle = canCastle(king, rook, intervening, opposing)
+
+        king.checkMated = (inCheck && filteredMoves.length === 0)
 
         candidateMoves.moves = filteredMoves;
         delete candidateMoves.moveVectors; // no longer valid, and no longer needed
